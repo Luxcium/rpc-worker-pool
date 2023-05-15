70f27e21d2b23ff75e7e2896dff395444b4a6b97
Luxcium <42672814+Luxcium@users.noreply.github.com>
2023-05-15 02:01:17 -0400

    feat: ⚡️ Add wrap and unwrap function and types.
    Signed-off-by: Luxcium <42672814+Luxcium@users.noreply.github.com>

--- src/API/RPC-serialise.ts
+++ src/API/RPC-serialise.ts
@@ -11,6 +11,7 @@ import {
   RpcLeft,
   RpcNotification,
   RpcRequest,
+  RpcResponse,
   RpcResponseError,
   RpcRight,
 } from '../types/specs';
@@ -201,7 +202,7 @@ export function baseRpcNotification<N extends Array<any> | Record<string, any>>(
 }
 
 export function baseRpcResponseRight<R>(result: R) {
-  return (id: number): RpcRight<R> => ({
+  return (id: number | string): RpcRight<R> => ({
     jsonrpc: '2.0' as const,
     result,
     id,
@@ -224,3 +225,131 @@ export function baseRpcResponseError(code: number, message: string) {
     return baseRpcResponseLeft(responseError);
   };
 }
+
+type UnwrapedRpcError<E> = [
+  data: E | undefined,
+  code: number,
+  message: string,
+  rpcResponseError: RpcResponseError<E>
+];
+
+export function unwrapRpcError<E = unknown>(
+  rpcResponseError: RpcResponseError<E>
+) {
+  const { code, message, data } = rpcResponseError;
+  return [data, code, message, rpcResponseError] as UnwrapedRpcError<E>;
+}
+type UnwrapedRpcLeft<E> = [
+  error: RpcResponseError<E>,
+  unwrapedRpcError: UnwrapedRpcError<E>,
+  id: string | number | null,
+  jsonrpc: boolean,
+  rpcResponseLeft: RpcLeft<E>
+];
+export function unwrapRpcResponseLeft<E = unknown>(
+  rpcResponseLeft: RpcLeft<E>
+) {
+  const { error, id, jsonrpc } = rpcResponseLeft;
+  return [
+    error,
+    unwrapRpcError(error),
+    id,
+    jsonrpc === '2.0',
+    rpcResponseLeft,
+  ] as UnwrapedRpcLeft<E>;
+}
+
+type UnwrapedRpcRight<R> = [
+  result: R,
+  id: string | number | null,
+  jsonrpc: boolean,
+  rpcResponseRight: RpcRight<R>
+];
+
+export function unwrapRpcResponseRight<R = unknown>(
+  rpcResponseRight: RpcRight<R>
+) {
+  const { result, id, jsonrpc } = rpcResponseRight;
+  return [
+    result,
+    id,
+    jsonrpc === '2.0',
+    rpcResponseRight,
+  ] as UnwrapedRpcRight<R>;
+}
+
+type UnwrapedRpcRequest<Q extends any[] | Record<string, any> = any> = [
+  params: any,
+  method: string,
+  id: string | number | null,
+  jsonrpc: boolean,
+  rpcRequest: RpcRequest<Q>
+];
+
+export function unwrapRpcRequest<Q extends any[] | Record<string, any> = any>(
+  rpcRequest: RpcRequest<Q>
+) {
+  const { params, method, id, jsonrpc } = rpcRequest;
+  return [
+    params,
+    method,
+    id,
+    jsonrpc === '2.0',
+    rpcRequest,
+  ] as UnwrapedRpcRequest<Q>;
+}
+
+type UnwrapedRpcNotification<N extends any[] | Record<string, any> = any> = [
+  params: any,
+  method: string,
+  id: null,
+  jsonrpc: boolean,
+  rpcNotification: RpcNotification<N>
+];
+
+export function unwrapRpcNotification<
+  N extends any[] | Record<string, any> = any
+>(rpcNotification: RpcNotification<N>) {
+  const { params, method, jsonrpc } = rpcNotification;
+  return [
+    params,
+    method,
+    null,
+    jsonrpc === '2.0',
+    rpcNotification,
+  ] as UnwrapedRpcNotification<N>;
+}
+
+type UnwrapedRpcResponse<R, E = any> = [
+  result: R,
+  error: RpcResponseError<E> | null | undefined,
+  unwrapedRpcError: UnwrapedRpcError<E>,
+  id: string | number | null,
+  jsonrpc: boolean,
+  rpcResponseRight: RpcRight<R>
+];
+export function unwrapRpcResponse<R = unknown>(rpcResponse: RpcResponse<R>) {
+  const { result, error, id, jsonrpc } = rpcResponse;
+
+  return [
+    result,
+    error,
+    error ? unwrapRpcError(error) : null,
+    id,
+    jsonrpc === '2.0',
+    rpcResponse,
+  ] as UnwrapedRpcResponse<R>;
+}
+/*
+  return (params: N) =>
+    (_id: null): RpcNotification<N> => {
+      const notification = {
+        jsonrpc: '2.0' as const,
+        method,
+        params,
+      };
+      verbose && console.dir(notification, options);
+      return notification;
+    };
+}
+ */
