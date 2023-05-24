f0292ec6dc01573043434b13b77474e179ff4029
Luxcium <42672814+Luxcium@users.noreply.github.com>
2023-05-24 14:00:27 -0400

    Refactor pipeline code to TypeScript best practice
    Signed-off-by: Luxcium <42672814+Luxcium@users.noreply.github.com>

--- src/Pipe/Step.ts
+++ src/Pipe/Step.ts
@@ -1,39 +1,56 @@
-export interface IProcessStep<T, R> {
-  transform: (input: T) => R;
+export interface ITransformInput<T, R> {
   transformInput(input: T): R;
 }
-
-export abstract class BaseProcessStep<T, R> implements IProcessStep<T, R> {
-  constructor(public transform: (input: T) => R) {}
-
-  transformInput(input: T): R {
-    return this.transform(input);
+export interface ICompose<T, R> {
+  compose<U>(composeWith: (input: R) => U): ProcessStepComposable<T, U>;
+}
+export abstract class BaseProcessStep<T, R> {
+  constructor(protected _transform: (input: T) => R) {}
+  get transform() {
+    return this._transform;
   }
 }
-
-// export function bigString(str: string | bigint): string {
-// (str: string | bigint): string
 
 export class ProcessStep<T, R>
   extends BaseProcessStep<T, R>
-  implements IProcessStep<T, R>
+  implements ITransformInput<T, R>
 {
-  constructor(transform: (input: T) => R) {
+  static of<TVal, RVal>(transform: (input: TVal) => RVal) {
+    return new ProcessStep<TVal, RVal>(transform);
+  }
+  static from<TVal, RVal>(processStep: BaseProcessStep<TVal, RVal>) {
+    return new ProcessStep<TVal, RVal>(processStep.transform);
+  }
+  protected constructor(transform: (input: T) => R) {
     super(transform);
   }
+  transformInput(input: T): R {
+    return this._transform(input);
+  }
 }
-
-export class NumericProcessStep
-  extends BaseProcessStep<number, number>
-  implements IProcessStep<number, number>
+export class ProcessStepComposable<T, R>
+  extends ProcessStep<T, R>
+  implements ITransformInput<T, R>, ICompose<T, R>
 {
-  constructor(transform: (input: number) => number) {
+  static override of<TVal, RVal>(transform: (input: TVal) => RVal) {
+    return new ProcessStepComposable<TVal, RVal>(transform);
+  }
+  static override from<TVal, RVal>(processStep: BaseProcessStep<TVal, RVal>) {
+    return new ProcessStepComposable<TVal, RVal>(processStep.transform);
+  }
+  protected constructor(transform: (input: T) => R) {
     super(transform);
+  }
+
+  public compose<U>(composeWith: (input: R) => U): ProcessStepComposable<T, U> {
+    const transform: (input: T) => R = this._transform;
+    const composedTransform = (input: T) => composeWith(transform(input));
+    return new ProcessStepComposable<T, U>(composedTransform);
   }
 }
 
 export class Pipeline<T, R> {
-  private constructor(private steps: ProcessStep<any, any>[]) {}
+  private constructor(private readonly steps: ProcessStep<any, any>[]) {}
 
   static initialize<T, R>(step: ProcessStep<T, R>): Pipeline<T, R> {
     return new Pipeline([step]);
@@ -51,91 +68,3 @@ export class Pipeline<T, R> {
     ) as R;
   }
 }
-
-export type MyObjectType = Record<string | number | symbol, any>;
-
-// export type O<T> = {};
-//
-export type T_A76FBD2<T> = T extends
-  | Function
-  | Array<any>
-  | Date
-  | RegExp
-  | Map<any, any>
-  | Set<any>
-  ? never
-  : T extends undefined | null | boolean | number | string | bigint | symbol
-  ? never
-  : T extends (...args: any[]) => any
-  ? never
-  : T extends object
-  ? {
-      [K in keyof T]: T[K] extends (...args: any[]) => any
-        ? never
-        : T_A76FBD2<T[K]>;
-    }
-  : T extends any[]
-  ? Array<T_A76FBD2<T[number]>>
-  : T;
-export type T_A76FBD<T> = T extends (...args: any[]) => any
-  ? never
-  : T extends { [key: PropertyKey]: any }
-  ? { [K in keyof T]: T_A76FBD<T[K]> }
-  : T extends any[]
-  ? Array<T_A76FBD<T[number]>>
-  : T;
-
-export type T_SimpleObject<T> =
-  // Exclude anything that's not a plain object
-  T extends
-    | Array<any>
-    | Function
-    | null
-    | undefined
-    | boolean
-    | number
-    | string
-    | bigint
-    | symbol
-    ? never
-    : // Exclude built-in non-plain objects
-    T extends
-        | Date
-        | RegExp
-        | Map<any, any>
-        | Set<any>
-        | WeakMap<any, any>
-        | WeakSet<any>
-    ? never
-    : // Allow only plain objects and apply the process recursively to its properties
-      { [K in keyof T]: T_ValidProperty<T[K]> };
-
-export type T_ValidProperty<T> =
-  // Exclude functions and built-in non-plain objects
-  T extends
-    | Function
-    | Date
-    | RegExp
-    | Map<any, any>
-    | Set<any>
-    | WeakMap<any, any>
-    | WeakSet<any>
-    ? never // Allow only primitive types, plain objects, and arrays of these (recursively)
-    : T extends undefined | null | boolean | number | string | bigint | symbol
-    ? T
-    : T extends Array<infer U>
-    ? Array<T_ValidProperty<U>>
-    : T extends object
-    ? T_SimpleObject<T>
-    : never;
-
-type Json = typeof JSON;
-export type _Never_d2 = T_A76FBD2<{ item: 10 }>; // both same
-export type _Never_d = T_A76FBD<Json>; // both same
-
-const multiplyByTwo = new ProcessStep<number, number>(n => n * 2);
-const addOne = new ProcessStep<number, number>(n => n + 1);
-
-const pipeline = Pipeline.initialize(multiplyByTwo).appendStep(addOne);
-
-console.log(pipeline.process(3)); // Outputs: 7
