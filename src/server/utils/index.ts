2d435cc23010a52342b83c899af81f1fe019e41b
Luxcium (Benjamin Vincent) <42672814+Luxcium@users.noreply.github.com>
2025-03-29 15:13:59 -0400

    refactor: Organize exports in index.ts for improved clarity and structure

--- src/server/utils/index.ts
+++ src/server/utils/index.ts
@@ -4,12 +4,15 @@ export { maxSize } from './maxSize';
 export { response } from './response';
 export { serverResponse } from './serverResponse';
 
-
 export {
+  getStrategy,
+  isStrategy,
   LEASTBUSY,
   RANDOM,
-  ROUNDROBIN, getStrategy,
-  isStrategy, strategies,
-  supportedStrategies
+  ROUNDROBIN,
+  strategies,
+  supportedStrategies,
 } from './strategies';
 export type { Leastbusy, Random, Roundrobin, Strategies } from './strategies';
+
+export * as errorHttp from './errorHttp';
